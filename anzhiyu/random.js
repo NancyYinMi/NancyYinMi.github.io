41c11f50365b85d9d395053eb0fc39cef87096f0
NancyYinMi <2011736047@qq.com>
2025-07-01 16:13:29 +0800

    Site updated: 2025-07-01 16:13:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/01/计组笔记第一章/"];function toRandomPost(){
+var posts=["2025/07/01/CO1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
